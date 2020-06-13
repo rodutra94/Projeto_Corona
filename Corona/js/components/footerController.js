b80718d3a81d3416ab80682a3101d2dd5c053aa0
Marcelo-Carlos <Marcelo Carlos>
2020-06-13 00:40:11 -0300

    teste

---
+++ Corona/js/components/footerController.js
@@ -0,0 +1,113 @@
+angular.module('rodapeElit').component('footerElit', {
+    template: '<!-- Footer -->' +
+        '<footer class="page-footer font-small mdb-color pt-4">' +
+
+        '<!-- Footer Links -->' +
+        '<div class="container text-center text-md-left">' +
+
+        '<!-- Footer links -->' +
+        '<div class="row text-center text-md-left mt-3 pb-3">' +
+
+        '<!-- Grid column -->' +
+        '<div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">' +
+        '<div id="footlogo">' +
+        '<div class="logo"><img src="IMAGENS/Elit-logo.png" alt="Logo Elit" Filmes"></div>' +
+        '<h6 class="text-uppercase mb-4 font-weight-bold">Elit Filmes</h6>' +
+        '</div>' +
+        '<p> elit.</p>' +
+        '</div>' +
+        '<!-- Grid column -->' +
+
+        '<hr class="w-100 clearfix d-md-none">' +
+
+        '<!-- Grid column -->' +
+        '<div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">' +
+        '<h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>' +
+        '<p>' +
+        '<a href="Termos_Uso.html">Termos de uso</a>' +
+        '</p>' +
+        '<p>' +
+        '<a href="Taxas.html">Taxas</a>' +
+        '</p>' +
+        '<p>' +
+        '<a href="Como_funciona.html">Como funciona?</a>' +
+        '</p>' +
+        '<p>' +
+        '<a href="Fale_conosco.html">Fale conosco</a>' +
+        '</p>' +
+        '</div>' +
+        '<!-- Grid column -->' +
+
+        '<hr class="w-100 clearfix d-md-none">' +
+
+        '<!-- Grid column -->' +
+        '<div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">' +
+        '<p>' +
+        '<a href="Politica_Cancelamento.html">Política de Cancelamento</a>' +
+        '</p>' +
+        '<p>' +
+        '<a href="Politica_Privacidade.html">Política de Privacidade</a>' +
+        '</p>' +
+        '</div>' +
+
+        '</div>' +
+        '<!-- Footer links -->' +
+
+        '<hr>' +
+
+        '<!-- Grid row -->' +
+        '<div class="row d-flex align-items-center">' +
+
+        '<!-- Grid column -->' +
+        '<div class="col-md-7 col-lg-8">' +
+
+        '<!--Copyright-->' +
+        '<p class="text-center text-md-left">© 2018 Copyright:' +
+        '<a href="index.html">' +
+        '<strong> Elit Filmes</strong>' +
+        '</a>' +
+        '</p>' +
+
+        '</div>' +
+        '<!-- Grid column -->' +
+
+        '<!-- Grid column -->' +
+        '<div class="col-md-5 col-lg-4 ml-lg-0">' +
+
+        '<!-- Social buttons -->' +
+        '<div class="text-center text-md-right">' +
+        '<ul class="list-unstyled list-inline">' +
+        '<li class="list-inline-item">' +
+        '<a class="btn-floating btn-sm rgba-white-slight mx-1">' +
+        '<i class="fab fa-facebook-f"></i>' +
+        '</a>' +
+        '</li>' +
+        '<li class="list-inline-item">' +
+        '<a class="btn-floating btn-sm rgba-white-slight mx-1">' +
+        '<i class="fab fa-twitter"></i>' +
+        '</a>' +
+        '</li>' +
+        '<li class="list-inline-item">' +
+        '<a class="btn-floating btn-sm rgba-white-slight mx-1">' +
+        '<i class="fab fa-google-plus-g"></i>' +
+        '</a>' +
+        '</li>' +
+        '<li class="list-inline-item">' +
+        '<a class="btn-floating btn-sm rgba-white-slight mx-1">' +
+        '<i class="fab fa-linkedin-in"></i>' +
+        '</a>' +
+        '</li>' +
+        '</ul>' +
+        '</div>' +
+
+        '</div>' +
+        '<!-- Grid column -->' +
+
+        '</div>' +
+        '<!-- Grid row -->' +
+
+        '</div>' +
+        '<!-- Footer Links -->' +
+
+        '</footer>'
+});
